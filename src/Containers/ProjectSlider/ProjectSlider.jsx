68d28eb036ca8186609fc80d83b6051d65231480
Lylouf13 <lylian.leutard@gmail.com>
2024-06-12 16:19:46 +0200

    improved slider effect and refactored some code

--- src/Containers/ProjectSlider/ProjectSlider.jsx
+++ src/Containers/ProjectSlider/ProjectSlider.jsx
@@ -10,7 +10,22 @@ export default function ProjectSlider() {
   const [startX, setStartX] = useState(null)
   const [scrollLeft, setScrollLeft] = useState(null)
   const slider = useRef()
+  const changedCards = []
 
+
+  // Reset temporary classes from both slider and cards
+  // These classes are added to give better UX on drag&drop 
+  // and avoid accidental clicking while rolling the slider
+  function resetStates(){
+    changedCards.forEach((e) =>{
+      e.offsetParent.className = 'projectCard'
+    })
+    changedCards.length = 0
+    if(slider.current.className !== 'projectSlider'){
+      slider.current.className = 'projectSlider'
+    }
+  }
+  
   const handleMouseDown = (e) =>{
     setIsDown(true)
     setStartX(e.pageX - slider.current.offsetLeft)
@@ -18,13 +33,23 @@ export default function ProjectSlider() {
   }
   const handleMouseUp = () =>{
     setIsDown(false)
+    resetStates()
   }
   const handleMouseLeave = () =>{
     setIsDown(false)
+    resetStates()
   }
 
   const handleMouseMove = (e) =>{
     if(!isDown) return
+    if(e.target.offsetParent.className === 'projectCard')
+    {
+      e.target.offsetParent.className += ' projectCard-broken -active'
+      changedCards.push(e.target)
+    }
+    if(slider.current.className === 'projectSlider'){
+      slider.current.className += ' -active'
+    }
     e.preventDefault();
     const x = e.pageX - slider.current.offsetLeft
     const walk = (x - startX) * 1.5
