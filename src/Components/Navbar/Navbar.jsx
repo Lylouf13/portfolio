f6379cf44d6f03d47ec84e1699f397ec8f3a88c2
Lylouf13 <lylian.leutard@gmail.com>
2024-07-01 16:38:26 +0200

    added current page on navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './navbar.scss'
 
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Navbar() {
+  const location = useLocation()
   return (
     <nav className='navbar'>
-        <Link to="/" className='navbar__link'>Home</Link>
-        <Link to="/web" className='navbar__link'>Web</Link>
-        <Link to="/games" className='navbar__link'>Games</Link>
-        <Link to="/pixelArt" className='navbar__link'>Pixel Art</Link>
+        <Link to="/" className={`navbar__link ${location.pathname==="/"? "navbar__link-current" : ""}`}>Home</Link>
+        <Link to="/web" className={`navbar__link ${location.pathname==="/web"? "navbar__link-current" : ""}`}>Web</Link>
+        <Link to="/games" className={`navbar__link ${location.pathname==="/games"? "navbar__link-current" : ""}`}>Games</Link>
+        <Link to="/pixelArt" className={`navbar__link ${location.pathname==="/pixelArt"? "navbar__link-current" : ""}`}>Pixel Art</Link>
     </nav>
   )
 }
