6e77e5f0828ffbdcf943ea01d442bf056334219c
Lylouf13 <lylian.leutard@gmail.com>
2024-07-03 14:22:46 +0200

    Fixed some error on pixelCard css and added titles to PA json

--- src/Components/PixelCard/PixelCard.jsx
+++ src/Components/PixelCard/PixelCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './pixelCard.scss'
 
 export default function PixelCard(data) {
+
   return (
     // Cards get "pixelGrid__type-id" as an extra class for grid construction purposes, which is done in pixelGrid.scss
     <div className={`pixelCard pixelGrid__${data.type}-${data.id}`}>
