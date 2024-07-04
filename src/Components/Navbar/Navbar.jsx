656dd6f447f41edf8933a361236ba68184a65a64
Lylouf13 <lylian.leutard@gmail.com>
2024-07-04 18:21:55 +0200

    removed route changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
   const location = useLocation()
   return (
     <nav className='navbar'>
-        <Link to="/portfolio" className={`navbar__link ${location.pathname==="/portfolio"? "navbar__link-current" : ""}`}>Home</Link>
-        <Link to="/portfolio/web" className={`navbar__link ${location.pathname.includes("/web")? "navbar__link-current" : ""}`}>Web</Link>
-        <Link to="/portfolio/games" className={`navbar__link ${location.pathname.includes("/games")? "navbar__link-current" : ""}`}>Games</Link>
-        <Link to="/portfolio/pixelArt" className={`navbar__link ${location.pathname==="/portfolio/pixelArt"? "navbar__link-current" : ""}`}>Pixel Art</Link>
+        <Link to="/" className={`navbar__link ${location.pathname==="/"? "navbar__link-current" : ""}`}>Home</Link>
+        <Link to="/web" className={`navbar__link ${location.pathname.includes("/web")? "navbar__link-current" : ""}`}>Web</Link>
+        <Link to="/games" className={`navbar__link ${location.pathname.includes("/games")? "navbar__link-current" : ""}`}>Games</Link>
+        <Link to="/pixelArt" className={`navbar__link ${location.pathname==="/pixelArt"? "navbar__link-current" : ""}`}>Pixel Art</Link>
     </nav>
   )
 }
