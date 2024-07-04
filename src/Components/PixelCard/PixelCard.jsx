07d20d1cd1345af7bd162abd7ada10edcc2aeaa0
Lylouf13 <lylian.leutard@gmail.com>
2024-07-04 08:41:34 +0200

    added non draggable to pixelCards

--- src/Components/PixelCard/PixelCard.jsx
+++ src/Components/PixelCard/PixelCard.jsx
@@ -5,9 +5,9 @@ export default function PixelCard(data) {
 
   return (
     // Cards get "pixelGrid__type-id" as an extra class for grid construction purposes, which is done in pixelGrid.scss
-    <div className={`pixelCard pixelGrid__${data.type}-${data.id}`}>
-        <h2 className='pixelCard__title'>{data.title}</h2>
-        <img className='pixelCard__image' src={data.image} alt="" />
+    <div draggable='false' className={`pixelCard pixelGrid__${data.type}-${data.id}`}>
+        <h2 draggable='false' className='pixelCard__title'>{data.title}</h2>
+        <img draggable='false' className='pixelCard__image' src={data.image} alt="" />
     </div>
   )
 }
