9212faff211bc8a38a91985a2f7a9f405121b1ca
Lylouf13 <lylian.leutard@gmail.com>
2024-07-05 18:50:30 +0200

    modified navbar location exception

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
   const location = useLocation()
   return (
     <nav className='navbar'>
-        <Link to="/Portfolio" className={`navbar__link ${location.pathname==="/"? "navbar__link-current" : ""}`}>Home</Link>
+        <Link to="/Portfolio" className={`navbar__link ${location.pathname==="https://lylouf13.github.io/Portfolio"? "navbar__link-current" : ""}`}>Home</Link>
         <Link to="/Portfolio/web" className={`navbar__link ${location.pathname.includes("/web")? "navbar__link-current" : ""}`}>Web</Link>
         <Link to="/Portfolio/games" className={`navbar__link ${location.pathname.includes("/games")? "navbar__link-current" : ""}`}>Games</Link>
-        <Link to="/Portfolio/pixelArt" className={`navbar__link ${location.pathname==="/pixelArt"? "navbar__link-current" : ""}`}>Pixel Art</Link>
+        <Link to="/Portfolio/pixelArt" className={`navbar__link ${location.pathname.includes("/pixelArt")? "navbar__link-current" : ""}`}>Pixel Art</Link>
     </nav>
   )
 }
