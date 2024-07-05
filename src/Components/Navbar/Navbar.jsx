2500bff53d3f4bd4e5fb428ddf88dfb38c93318b
Lylouf13 <lylian.leutard@gmail.com>
2024-07-05 19:00:41 +0200

    changed navbar location exception, again

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,10 @@ import { Link, useLocation } from 'react-router-dom'
 
 export default function Navbar() {
   const location = useLocation()
+  console.log(location)
   return (
     <nav className='navbar'>
-        <Link to="/Portfolio" className={`navbar__link ${location.pathname==="https://lylouf13.github.io/Portfolio"? "navbar__link-current" : ""}`}>Home</Link>
+        <Link to="/Portfolio" className={`navbar__link ${location.pathname==="/Portfolio"? "navbar__link-current" : ""}`}>Home</Link>
         <Link to="/Portfolio/web" className={`navbar__link ${location.pathname.includes("/web")? "navbar__link-current" : ""}`}>Web</Link>
         <Link to="/Portfolio/games" className={`navbar__link ${location.pathname.includes("/games")? "navbar__link-current" : ""}`}>Games</Link>
         <Link to="/Portfolio/pixelArt" className={`navbar__link ${location.pathname.includes("/pixelArt")? "navbar__link-current" : ""}`}>Pixel Art</Link>
