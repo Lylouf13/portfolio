60ee2bb9c865e17bd7abce7374bdf2e01b0b448b
Lylouf13 <lylian.leutard@gmail.com>
2024-07-06 10:14:34 +0200

    Added support to tab navigation / interaction in pixelGrids

--- src/Components/PixelCard/PixelCard.jsx
+++ src/Components/PixelCard/PixelCard.jsx
@@ -3,10 +3,16 @@ import './pixelCard.scss'
 
 export default function PixelCard(data) {
 
+  const handleKeyDown = (e) =>{
+    if(e.key==="Enter"){
+      data.setViewer(data.image, data.title)
+    }
+  }
+
   return (
     // Cards get "pixelGrid__type-id" as an extra class for grid construction purposes, which is done in pixelGrid.scss
     // Also opens ImageViewer and sends required data   
-    <div draggable='false' className={`pixelCard pixelGrid__${data.type}-${data.id} ${data.usable ? 'pixelCard-disabled':''}`}>
+    <div tabIndex="0" onKeyDown={handleKeyDown} draggable='false' className={`pixelCard pixelGrid__${data.type}-${data.id} ${data.usable ? 'pixelCard-disabled':''}`}>
         <h2 draggable='false' className='pixelCard__title'>{data.title}</h2>
         <img onClick={()=>data.setViewer(data.image, data.title)} draggable='false' className='pixelCard__image' src={data.image} alt="" />
     </div>
