ac09e673e17aed9da4296370d71e47366a8b81ee
Lylouf13 <lylian.leutard@gmail.com>
2024-07-11 10:52:01 +0200

    removed consolelog

--- src/Components/ProjectText/ProjectText.jsx
+++ src/Components/ProjectText/ProjectText.jsx
@@ -4,12 +4,11 @@ import './projectText.scss'
 export default function ProjectText(props) {
 
   const text = props.text.split('<br />')
-  console.log(text)
 
   return (
     <div className='projectText'>
         {props.title && <h2 className='projectText__title'>{props.title}</h2>}
-        {props.text && text.map((part, index) => <p key={`"line-"${index}`}className='projectText__text'>{part}</p>)}
+        {text.map((part, index) => <p key={`"line-"${index}`}className='projectText__text'>{part}</p>)}
     </div>
   )
 }
