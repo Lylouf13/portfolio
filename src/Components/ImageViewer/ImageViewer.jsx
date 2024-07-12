47e96df3f1c7920a8296232aa5d8c1c00d5622a0
Lylouf13 <lylian.leutard@gmail.com>
2024-07-12 13:24:51 +0200

    added delay option in anim mixin, fixed imageviewerTitle in mobile version

--- src/Components/ImageViewer/ImageViewer.jsx
+++ src/Components/ImageViewer/ImageViewer.jsx
@@ -4,9 +4,9 @@ import './imageViewer.scss'
 
 export default function ImageViewer(data) {
   return (
-    <div onClick={data.closeViewer} className={`viewer ${data.open ? 'viewer-open' : ''}`}>
+    <figure onClick={data.closeViewer} className={`viewer ${data.open ? 'viewer-open' : ''}`}>
         <img onClick={data.closeViewer} className='viewer__image' alt='dede' src={data.image}/>
         <figcaption onClick={data.closeViewer} className='viewer__title'>- {data.title} -</figcaption>
-    </div>
+    </figure>
   )
 }
