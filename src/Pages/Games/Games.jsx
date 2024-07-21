d573d9c0198923020d5beac716ee693a778c1aad
Lylouf13 <lylian.leutard@gmail.com>
2024-07-21 09:58:59 +0200

    rewrote games maintext

--- src/Pages/Games/Games.jsx
+++ src/Pages/Games/Games.jsx
@@ -17,8 +17,9 @@ useEffect(() => {
     <main>
       <section className='main'>
         <h1 className='main__title'>Games</h1>
-        <p className='main__text'>Si on demande au moi d'il y a 10 ans ce qu'il veut faire de sa vie,
-          il répondra probablement "des jeux vidéos !"
+        <p className='main__text'>Le point de départ de mon parcours professionnel a été ma formation en game design à l'ICAN,
+          durant laquelle j'ai pu m'initier à de nombreux concepts et outils, artistiques comme techniques. Ces projets sont soit des
+          projets étudiants, soit des projets personnels ou bien dans le contexte de game jams.
         </p>
       </section>
       <nav>
