7c349034a1b79be9dcfa9377c8ea11f917ecaf01
Lylouf13 <lylian.leutard@gmail.com>
2024-07-22 00:59:57 +0200

    updated CTAs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,13 @@ import { Link, useLocation } from 'react-router-dom'
 
 export default function Navbar() {
   const location = useLocation()
+  console.log(location)
   return (
     <nav className='navbar'>
-        <img className='navbar__logo' src={'/portfolio/Assets/Images/Misc/logo.png'} alt="mon logo" />
-        <Link to="/portfolio#top" className={`navbar__link ${location.pathname==="/portfolio"? "navbar__link-current" : ""}`}>Home</Link>
+        <Link to="/portfolio/#top">
+          <img className='navbar__logo' src={'/portfolio/Assets/Images/Misc/logo.png'} alt="mon logo" />
+        </Link>
+        <Link to="/portfolio/#top" className={`navbar__link ${location.pathname==="/portfolio/"? "navbar__link-current" : ""}`}>Home</Link>
         <Link to="/portfolio/web#top" className={`navbar__link ${location.pathname.includes("/web")? "navbar__link-current" : ""}`}>Web</Link>
         <Link to="/portfolio/games#top" className={`navbar__link ${location.pathname.includes("/games")? "navbar__link-current" : ""}`}>Games</Link>
         <Link to="/portfolio/pixelArt#top" className={`navbar__link ${location.pathname.includes("/pixelArt")? "navbar__link-current" : ""}`}>Pixel Art</Link>
